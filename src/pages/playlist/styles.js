63b6a0f6777a5dbb625cdb385b30c39eff0b5c61
Felipe Oliveira <felipeoliveiradeveloper@gmail.com>
2019-04-10 01:46:58 -0300

    correção styled.css

--- src/pages/playlist/styles.js
+++ src/pages/playlist/styles.js
@@ -90,7 +90,7 @@ export const SongItem = styled.tr`
     line-height: 40px;
     background: ${props => (props.selected ? '#282828' : 'transparent')};
     color: ${props => (props.playing ? '#1ed760' : '#fff')};
-    &:first-child
+    &:first-child {
       width: 80px;
       text-align: left;
     }
